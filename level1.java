73edd05422e378c74ba09ee9ef48d0738e62c87e
Bandy <tyler13@colostate.edu>
2026-02-01 14:59:42 -0700

    added thongs

--- level1.java
+++ level1.java
@@ -1,9 +1,10 @@
-
+import java.util.Scanner;
 
 public class Level1 implements levels {
     private String[][] map;
     private Bomb bomb = new Bomb("B");
     private User player = new User("x");
+    private PlayerControler playerControler = new PlayerControler(map, player.getPlayer(), 2, 1);
     
     @Override
     public void size(int length, int width) {
@@ -11,36 +12,62 @@ public class Level1 implements levels {
     }
 
     @Override
-    public void playerSpawn(String[][] location) {
-        location[2][1] = player.getPlayer();
+    public void playerSpawn(String[][] map) {
+        map[2][1] = player.getPlayer();
+    }
+
+    public void printBoard() {
+        for(int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++ ) {
+                System.out.print("[" + map[i][j] + "]");
+            }
+            System.out.println();
+        }
     }
 
     
     public void play() {
-    size(3,3);
+        //sets the size of level1
+        size(3,3);
 
-    for(int i = 0; i < map.length; i++) {
-        for(int j = 0; j < map[i].length; j++) {
-            map[i][j] = " ";
-        }
-    }
-
-    playerSpawn(map);
-
-    for(int i = 0; i < map.length; i++) {
-        for(int j = 0; j < map[i].length; j++) {
-            int result = bomb.explode();
-            if(result <= 2) {
-                map[i][j] = bomb.getBOMB();
+        //prints the map with emty values
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                map[i][j] = " ";
             }
-          }
-       }
-
-    for(int i = 0; i < map.length; i++) {
-        for(int j = 0; j < map[i].length; j++) {
-            System.out.print("[" + map[i][j] + "]");
-            }
-        System.out.println();
         }
+
+       
+        //randomly places the bombs on said map 
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                int result = bomb.explode();
+                if(result <= 2) {
+                    map[i][j] = bomb.getBOMB();
+                }
+            }
+        }
+        //print the board
+        printBoard();
+
+        //Game Loop
+        boolean playing = true;
+        while(playing) {
+            printBoard();
+            playerSpawn(map);
+            System.out.println("Enter your move (up, down, left, right): ");
+            Scanner scan = new Scanner("System.in");
+            String input = scan.nextLine();
+            if(input.equals("exit")) {
+                System.out.println("Thanks for Playing!");
+                playing = false;
+            } else if(input.equals("up") || input.equals("down")||input.equals("left")||input.equals("right")) {
+                playerControler.isValidPosition();
+                playerControler.playerMovement(input);
+                printBoard();
+
+            }
+        }
+
     }
 }
--- levels.java
+++ levels.java
@@ -2,4 +2,5 @@ public interface levels {
     void size(int length, int width);
 
     void playerSpawn(String[][] location);
+
 }
